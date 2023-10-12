b3a4f2ee307bf489bd927d77cd25db1825f91feb
CodeWizard-1 <>
2023-10-12 14:13:14 +0000

    Add pages, all works

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -3,9 +3,9 @@ from taskmanager import app, db
 from taskmanager.models import Category, Task
 app.app_context().push()
 
-# Теперь вы находитесь в контексте приложения и можете выполнить операции, требующие доступа к приложению
+
 db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
